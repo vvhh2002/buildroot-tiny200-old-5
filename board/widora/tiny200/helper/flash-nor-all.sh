2daa5a14f7dd894a942fa4a9c3960f8baedb57de
Aodzip <aodzip@gmail.com>
2020-09-20 00:32:00 +0800

    Add RW rootfs for NOR & NAND

--- board/widora/tiny200/helper/flash-nor-all.sh
+++ board/widora/tiny200/helper/flash-nor-all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sunxi-fel -p spiflash-write 0 output/images/sysimage-flash.img
+sunxi-fel -p spiflash-write 0 output/images/sysimage-nor.img
--- board/widora/tiny200/scripts/genimage.sh
+++ board/widora/tiny200/scripts/genimage.sh
@@ -10,5 +10,5 @@ SELFDIR=`dirname \`realpath ${0}\``
 
 ${SELFDIR}/mknanduboot.sh ${1}/${2} ${1}/u-boot-sunxi-with-nand-spl.bin
 support/scripts/genimage.sh ${1} -c board/widora/tiny200/genimage-sdcard.cfg
-support/scripts/genimage.sh ${1} -c board/widora/tiny200/genimage-flash.cfg
+support/scripts/genimage.sh ${1} -c board/widora/tiny200/genimage-nor.cfg
 support/scripts/genimage.sh ${1} -c board/widora/tiny200/genimage-nand.cfg
